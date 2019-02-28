1861adcedcc25519e5bc72382b8b6dff897f1dc0
Carlos Sales <raxuk8@gmail.com>
2019-02-28 03:37:27 +0100

    first commit

---
+++ conv.py
@@ -0,0 +1,15 @@
+from PIL import Image
+import os, sys
+
+basewidth = 512
+i = 0
+
+for filename in os.listdir(os.path.dirname(os.path.abspath(__file__))):
+	if filename.endswith(".jpg"):
+		img = Image.open(filename)
+		wpercent = (basewidth/float(img.size[0]))
+		hsize = int((float(img.size[1])*float(wpercent)))
+		img = img.resize((basewidth,hsize), Image.ANTIALIAS)
+		img.save(str(i)+'.png') 
+		os.remove(filename)
+		i += 1
